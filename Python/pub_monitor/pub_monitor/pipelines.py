dfe3a65b4d642f0be5c9f0ccbf8a3c768cb28101
alwingeorge48 <61450555+alwingeorge48@users.noreply.github.com>
2020-11-13 13:04:19 +0530

    Update pipelines.py
    Test3

--- Python/pub_monitor/pub_monitor/pipelines.py
+++ Python/pub_monitor/pub_monitor/pipelines.py
@@ -3,7 +3,6 @@
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# okay
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
 from scrapy.exporters import CsvItemExporter
